fd52cbdb79ca07b4997cb7b2e67d66a2e265d542
Radhika Jasra <95180021+rjasra@users.noreply.github.com>
2024-12-18 02:08:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,108 +1,64 @@
 import React, { useState } from "react";
-import Toolbar from "./components/Toolbar";
-import Workspace from "./components/Workspace";
-import Summary from "./components/Summary";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useNavigate,
+} from "react-router-dom";
+import Mitm from "./components/mitm";
+import Bruteforce from "./components/bruteforce";
+import Custompage from "./components/custompage";
 
-function App() {
-  const [elements, setElements] = useState([]);
-  const [connections, setConnections] = useState([]);
-  const [isEraseMode, setIsEraseMode] = useState(false);
-
-  const addElement = (element) => {
-    setElements((prevElements) => [...prevElements, element]);
-  };
-
-  const addConnection = (connection) => {
-    setConnections((prevConnections) => [...prevConnections, connection]);
-  };
-
-  const deleteConnection = (index) => {
-    setConnections((prevConnections) => {
-      const updatedConnections = prevConnections.filter((_, i) => i !== index);
-
-      // Get the elements that have connections
-      const elementsWithConnections = updatedConnections.flatMap(
-        (connection) => [connection.start.element.id, connection.end.element.id]
-      );
-      const uniqueElementsWithConnections = [
-        ...new Set(elementsWithConnections),
-      ];
-
-      // Filter out elements that have no connections
-      const updatedElements = elements.filter((element) =>
-        uniqueElementsWithConnections.includes(element.id)
-      );
-
-      setElements(updatedElements);
-
-      return updatedConnections;
-    });
-  };
-
-  const toggleEraseMode = () => {
-    setIsEraseMode((prevMode) => !prevMode);
-  };
+function MainPage() {
+  const [showPredefinedOptions, setShowPredefinedOptions] = useState(false);
+  const navigate = useNavigate();
 
   return (
-    <div
-      style={{
-        display: "flex",
-        height: "100vh",
-        fontFamily: "Arial, sans-serif",
-        position: "relative",
-      }}
-    >
-      <div
-        style={{
-          padding: "10px",
-          backgroundColor: "#e0e0e0",
-          borderBottom: "1px solid #ccc",
-          fontSize: "14px",
-          color: "#333",
-          position: "absolute",
-          top: "0",
-          left: "0",
-          width: "100%",
-          height: "20px",
-          zIndex: "10",
-          textAlign: "left",
-        }}
-      >
-        Security Lab
-      </div>
-      <div
-        style={{
-          display: "flex",
-          width: "100%",
-          height: "calc(100vh - 40px)",
-          marginTop: "40px",
-        }}
-      >
-        <Toolbar
-          onDropElement={addElement}
-          toggleEraseMode={toggleEraseMode}
-          isEraseMode={isEraseMode}
-        />
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            flex: 1,
-            overflow: "auto",
-          }}
-        >
-          <Workspace
-            elements={elements}
-            connections={connections}
-            addConnection={addConnection}
-            deleteConnection={deleteConnection}
-            isEraseMode={isEraseMode}
-            setElements={setElements} // Pass setElements here
-          />
+    <div className="main-page">
+      <div className="container">
+        <h1>Create, Test and Smiulate</h1>
+        <h2>all in one place</h2>
+        <div>
+          <button onClick={() => navigate("/custompage")}>
+            Custom Network
+          </button>
+
+          <button onClick={() => setShowPredefinedOptions(true)}>
+            Predefined Network
+          </button>
         </div>
-        <Summary />
+
+        {showPredefinedOptions && (
+          <div>
+            <button onClick={() => navigate("/mitm")}>MITM</button>
+
+            <button onClick={() => navigate("/bruteforce")}>Brute Force</button>
+
+            <button
+              className="back-button"
+              onClick={() => setShowPredefinedOptions(false)}
+            >
+              Go Back
+            </button>
+          </div>
+        )}
       </div>
     </div>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/custompage" element={<Custompage />} />
+        <Route path="/mitm" element={<Mitm />} />
+        <Route path="/bruteforce" element={<Bruteforce />} />
+      </Routes>
+    </Router>
   );
 }
 
